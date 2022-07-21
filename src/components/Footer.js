4536fb1099b84c65a6474f2ffc401c0adb112851
Stephen Fudge <sfudge@gmail.com>
2022-07-20 21:01:27 -0400

    added github and linkedin icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import './Footer.css';
-
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Footer = () => {
     return(
         <div className="footer">
-         <a className='footer-link' href="https://github.com/stephenfudge" target="_blank">GitHub</a>
+         <a className='footer-link' href="https://github.com/stephenfudge" target="_blank"><GitHubIcon fontSize='large' />GitHub</a>
 
-         <a className='footer-link' href="https://www.linkedin.com/in/stephenfudge/" target="_blank">LinkedIn</a>
+         <a className='footer-link' href="https://www.linkedin.com/in/stephenfudge/" target="_blank"><LinkedInIcon fontSize='large' />LinkedIn</a>
         </div>
     )
 };
