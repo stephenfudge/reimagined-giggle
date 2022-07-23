59036a1053fc7aad1f0a35df0e42426ae51e3207
Stephen Fudge <sfudge@gmail.com>
2022-07-23 16:49:42 -0400

    got rid of white background around projects. trying to decide on header text color

--- src/pagesSemantic/Projects.js
+++ src/pagesSemantic/Projects.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
+import './Projects.css';
 
-// import './Projects.css';
 
 import weather from '../images/work/weather.png';
 import quiz from '../images/work/quiz.png';
@@ -13,69 +13,69 @@ import pwGen from '../images/work/pwgen.png';
 
 export default function Projects() {
   return (
-   <>
-<div class="ui stackable three column grid container">
+   <div>
+<div className="ui stackable three column grid container">
   
-  <div class="column">
-    <div class="ui segment">
-    <h2 class="project-name">Weather Dashboard</h2><br/>
-                <img class="project-images" src={weather} alt="A Weather Dashboard"/><br/>
-                <a href="https://stephenfudge.github.io/stunning-robot/" target="_blank">See it live!</a><br/>
-                <a href="https://github.com/stephenfudge/stunning-robot" target="_blank">Take a look at the GitHub Repository</a>
-    </div>
+  <div className="column center aligned">
+    {/* <div className="ui segment> */}
+    <h2 className="ui header project-name"><span className="project-name">Weather Dashboard</span></h2><br/>
+                <img className="project-images" src={weather} alt="A Weather Dashboard"/><br/>
+                <a href="https://stephenfudge.github.io/stunning-robot/" target="_blank"><span className='project-links'>See it live!</span></a><br />
+                <a href="https://github.com/stephenfudge/stunning-robot" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
+    {/* </div> */}
 </div>
   
-  <div class="column">
-    <div class="ui segment">
-    <h2 class="project-name">Password Generator</h2><br/>
-                <img class="project-images" src={pwGen} alt="Password Generator Page" /><br/>
-                <a href="https://stephenfudge.github.io/turbo-robot/" target="_blank">See it live!</a><br/>
-                <a href="https://github.com/stephenfudge/turbo-robot" target="_blank">Take a look at the GitHub Repository</a>   
-    </div>
+  <div className="column center aligned">
+    {/* <div className="ui segment imagebackground"> */}
+    <h2 className="ui header"><span className="project-name">Password Generator</span></h2><br/>
+                <img className="project-images" src={pwGen} alt="Password Generator Page" /><br/>
+                <a href="https://stephenfudge.github.io/turbo-robot/" target="_blank"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://github.com/stephenfudge/turbo-robot" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a>   
+    {/* </div> */}
   </div>
   
-  <div class="column">
-    <div class="ui segment">
-    <h2 class="project-name">9-5 Day Planner</h2><br/>
-                <img class="project-images" src={dayPlanner} alt="9-5 Day Planner" /><br/>
-                <a href="https://stephenfudge.github.io/glowing-octo-waffle/" target="_blank">See it live!</a><br/>
-                <a href="https://github.com/stephenfudge/glowing-octo-waffle" target="_blank">Take a look at the GitHub Repository</a>   
-    </div>
+  <div className="column center aligned">
+    {/* <div className="ui segment"> */}
+    <h2 className="ui header project-name"><span className="project-name">9-5 Day Planner</span></h2><br/>
+                <img className="project-images" src={dayPlanner} alt="9-5 Day Planner" /><br/>
+                <a href="https://stephenfudge.github.io/glowing-octo-waffle/" target="_blank"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://github.com/stephenfudge/glowing-octo-waffle" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a>  
+    {/* </div> */}
 </div>
 
 </div>
 
-<div class="ui stackable three column grid container">
-  <div class="column">
-    <div class="ui segment">
-    <h2 class="project-name">JavaScript Quiz</h2><br/>
-                <img class="project-images" src={quiz} alt="Javascript Quiz" /><br/>
-                <a href="https://stephenfudge.github.io/furry-umbrella/" target="_blank">See it live!</a><br/>
-                <a href="https://github.com/stephenfudge/furry-umbrella" target="_blank">Take a look at the GitHub Repository</a>
-    </div>
+<div className="ui stackable three column grid container">
+  <div className="column center aligned">
+    {/* <div className="ui segment"> */}
+    <h2 className="ui header project-name"><span className="project-name">JavaScript Quiz</span></h2><br/>
+                <img className="project-images" src={quiz} alt="Javascript Quiz" /><br/>
+                <a href="https://stephenfudge.github.io/furry-umbrella/" target="_blank"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://github.com/stephenfudge/furry-umbrella" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
+    {/* </div> */}
   </div>
 
-  <div class="column">
-    <div class="ui segment">
-    <h2 class="project-name">Conversation Starters</h2><br/>
-                <img class="project-images" src={conversations} alt="Random Conversation Starters" /><br/>
-                <a href="https://github.com/danielfu13/flying-kittens17" target="_blank">See it live!</a><br/>
-                <a href="https://danielfu13.github.io/flying-kittens17/" target="_blank">Take a look at the GitHub Repository</a>
-    </div>
+  <div className="column center aligned">
+    {/* <div className="ui segment"> */}
+    <h2 className="ui header project-name"><span className="project-name" id="title2">Conversation Starters</span></h2><br/>
+                <img className="project-images" src={conversations} alt="Random Conversation Starters" /><br/>
+                <a href="https://github.com/danielfu13/flying-kittens17" target="_blank"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://danielfu13.github.io/flying-kittens17/" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
+    {/* </div> */}
   </div>
 
-  <div class="column">
-    <div class="ui segment">
-    <h2 class="project-name">FIFA Fan Site</h2><br/>
-                <img class="project-images" src={fifa} alt="FIFA Fan Site" /><br/>
-                <a href="https://vigilant-octo-lamp.herokuapp.com/" target="_blank">See it live!</a><br/>
-                <a href="https://github.com/stephenfudge/vigilant-octo-lamp" target="_blank">Take a look at the GitHub Repository</a>
-    </div>
+  <div className="column center aligned">
+    {/* <div className="ui segment"> */}
+    <h2 className="ui header project-name"><span className="project-name" id="title3">FIFA Fan Site</span></h2><br/>
+                <img className="project-images" src={fifa} alt="FIFA Fan Site" /><br/>
+                <a href="https://vigilant-octo-lamp.herokuapp.com/" target="_blank"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://github.com/stephenfudge/vigilant-octo-lamp" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
+    {/* </div> */}
   </div>
 
 
 </div>
-</>
+</div>
   );
 }
 
