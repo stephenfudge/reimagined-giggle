911e82281d086ad3a28e964e71e7d41b7eebd444
Stephen Fudge <sfudge@gmail.com>
2022-07-24 15:17:27 -0400

    fixed the resume page

--- src/pagesSemantic/Resume.js
+++ src/pagesSemantic/Resume.js
@@ -14,30 +14,24 @@ const Resume = () =>{
    </div>
       <div class="column">
    <span className='list-text' >
-    <span className='ui big text' id="title">Technical Skills</span>
-    <br />
-          <span className='ui large text'><span className='headings'>Languages: </span><span> JavaScript</span>
+    <h1 className='ui header' id="title">Technical Skills</h1>
+          <h3 className='ui header'><span className='headings'>Languages: </span><span className="list-text"> JavaScript, HTML5, CSS3</span>
           <br />
-          <span className='headings'> Tools/Frameworks/Libraries:</span></span>
-           <br />
+          <span className='headings'> Tools/Frameworks/Libraries:</span></h3>
            <ul id="list">
-            <span className='ui medium text'>
+            {/* <span className='ui medium text'> */}
             <li>React</li>
             <li>MongoDB</li>
             <li>MySQL</li>
             <li>GraphQL</li>
-            <li>OOP</li>
             <li>NodeJS</li>
             <li>Express.JS</li>
             <li>Object Relational Mapping</li>
-            <li> HTML5</li>
-            <li>CSS3</li>
+            <li>OOP</li>
             <li>Bootstrap</li>
             <li>Bulma</li>
-            <li></li>
-            <li></li>
-            <li></li>
-            </span>
+            <li>Handlebars.JS</li>
+            <li>Fomantic-UI</li>
            </ul>
            </span>
             <br />
