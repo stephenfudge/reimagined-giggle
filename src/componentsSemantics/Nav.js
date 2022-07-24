ac667a6a4867801666129b4dc3269fec106512e8
Stephen Fudge <sfudge@gmail.com>
2022-07-24 15:27:04 -0400

    added comments to css files

--- src/componentsSemantics/Nav.js
+++ src/componentsSemantics/Nav.js
@@ -1,4 +1,3 @@
-
 import './Nav.css';
 import name from '../images/name_nobg.png';
 
@@ -11,15 +10,16 @@ const Nav = (props) =>{
    }
    
     return (
-      <div id="testingcolor">
+     
 
         <div className="ui grid stackable menu container" id="navbar">
+        
           <div className="twelve wide stretched column" >
              <img src={name} alt="Logo of my name"/>
             </div>
   
           <div className="four wide column">
-    <div className="ui vertical fluid right menu links" id="navbar">
+    <div className="ui vertical fluid right menu links" id="navbar-links">
     <span className="links">
       <a className="item" id="about" onClick={pageChange}>
       <span className="link"> About Me </span>
@@ -38,40 +38,8 @@ const Nav = (props) =>{
   </div>
      
 </div>
-</div>
 
     )
 };
 
 export default Nav;
-
-
-
-// <div className="ui stackable menu">
-// <div className="item">
-//   <img src={name} />
-// </div>
-// <a className="item" id="about" onClick={pageChange}>About Me</a>
-// <a className="item" id="projects" onClick={pageChange}>Projects</a>
-// <a className="item" id="resume" onClick={pageChange}>Resume</a>
-// <a className="item" id="contact" onClick={pageChange}>Contact Me</a>
-// </div>
-
-
-
-// <div className="four wide column">
-// <div className="ui vertical fluid right tabular menu" id="links">
-//   <a className="item" id="about" onClick={pageChange}>
-//  <span className="links"> About Me </span>  
-//   </a>
-//   <a className="item" id="projects" onClick={pageChange}>
-//     <span className="links"> Projects</span>
-//   </a>
-//   <a className="item" id="resumes" onClick={pageChange}>
-//     <span className="links"> Resume</span>
-//   </a>
-//   <a className="item" id="contact" onClick={pageChange}>
-//      <span className="links">Contact Me</span>
-//   </a>
-// </div>
-// </div>
