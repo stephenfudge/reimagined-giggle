38d9a286fdd6de4ba39b90c3ee9bd92cb9e5f252
Stephen Fudge <sfudge@gmail.com>
2022-07-24 15:36:31 -0400

    added additional coding to footer and nav.css

--- src/componentsSemantics/Footer.js
+++ src/componentsSemantics/Footer.js
@@ -1,18 +1,13 @@
 import './Footer.css';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Footer = () => {
     return(
         <div className="footer">
-         {/* <a className='footer-link' href="https://github.com/stephenfudge" target="_blank"><GitHubIcon fontSize='large' />GitHub</a> */}
-         <a className='footer-link' href="https://github.com/stephenfudge" target="_blank"> <i class="github icon"> GitHub</i></a>
+         <a className='footer-link' href="https://github.com/stephenfudge" target="_blank" rel="noopener noreferrer"> <i class="github icon"> GitHub</i></a>
 
-         {/* <a className='footer-link' href="https://www.linkedin.com/in/stephenfudge/" target="_blank"><LinkedInIcon fontSize='large' />LinkedIn</a> */}
-         <a className='footer-link' href="https://www.linkedin.com/in/stephenfudge/" target="_blank"><i class="linkedin icon">LinkedIn</i></a>
+         <a className='footer-link' href="https://www.linkedin.com/in/stephenfudge/" target="_blank" rel="noopener noreferrer"><i class="linkedin icon">LinkedIn</i></a>
 
-        <a className='footer-link' href="https://twitter.com/stephen_fudge" target="_blank"><i class="twitter icon">Twitter</i></a>
+        <a className='footer-link' href="https://twitter.com/stephen_fudge" target="_blank" rel="noopener noreferrer"><i class="twitter icon">Twitter</i></a>
         </div>
     )
 };
