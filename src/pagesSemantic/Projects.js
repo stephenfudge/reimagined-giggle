5ab0bfe182a7dbb67307185b43da50e9e2ef2719
Stephen Fudge <sfudge@gmail.com>
2022-07-24 22:06:27 -0400

    added noopener noreferrer to projects.js

--- src/pagesSemantic/Projects.js
+++ src/pagesSemantic/Projects.js
@@ -19,22 +19,22 @@ export default function Projects() {
   <div className="column center aligned">
        <h2 className="ui header project-name"><span className="project-name">Weather Dashboard</span></h2><br/>
                 <img className="project-images" src={weather} alt="A Weather Dashboard"/><br/>
-                <a href="https://stephenfudge.github.io/stunning-robot/" target="_blank"><span className='project-links'>See it live!</span></a><br />
-                <a href="https://github.com/stephenfudge/stunning-robot" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
+                <a href="https://stephenfudge.github.io/stunning-robot/" target="_blank" rel="noopener noreferrer"><span className='project-links'>See it live!</span></a><br />
+                <a href="https://github.com/stephenfudge/stunning-robot" target="_blank" rel="noopener noreferrer"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
   </div>
   
   <div className="column center aligned">
         <h2 className="ui header project-name"><span className="project-name">Conversation Starters</span></h2><br/>
                 <img className="project-images" src={conversations} alt="Random Conversation Starters" /><br/>
-                <a href="https://github.com/danielfu13/flying-kittens17" target="_blank"><span className='project-links'>See it live!</span></a><br/>
-                <a href="https://danielfu13.github.io/flying-kittens17/" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
+                <a href="https://github.com/danielfu13/flying-kittens17" target="_blank" rel="noopener noreferrer"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://danielfu13.github.io/flying-kittens17/" target="_blank" rel="noopener noreferrer"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
      </div>
   
   <div className="column center aligned">
         <h2 className="ui header project-name"><span className="project-name">9-5 Day Planner</span></h2><br/>
                 <img className="project-images" src={dayPlanner} alt="9-5 Day Planner" /><br/>
-                <a href="https://stephenfudge.github.io/glowing-octo-waffle/" target="_blank"><span className='project-links'>See it live!</span></a><br/>
-                <a href="https://github.com/stephenfudge/glowing-octo-waffle" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a>  
+                <a href="https://stephenfudge.github.io/glowing-octo-waffle/" target="_blank" rel="noopener noreferrer"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://github.com/stephenfudge/glowing-octo-waffle" target="_blank" rel="noopener noreferrer"><span className='project-links'>Take a look at the GitHub Repository</span></a>  
    </div>
 </div>
 
@@ -44,16 +44,16 @@ export default function Projects() {
   <div className="column center aligned">
        <h2 className="ui header"><span className="project-name">Password Generator</span></h2><br/>
                 <img className="project-images" src={pwGen} alt="Password Generator Page" /><br/>
-                <a href="https://stephenfudge.github.io/turbo-robot/" target="_blank"><span className='project-links'>See it live!</span></a><br/>
-                <a href="https://github.com/stephenfudge/turbo-robot" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a>   
+                <a href="https://stephenfudge.github.io/turbo-robot/" target="_blank" rel="noopener noreferrer"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://github.com/stephenfudge/turbo-robot" target="_blank" rel="noopener noreferrer"><span className='project-links'>Take a look at the GitHub Repository</span></a>   
      </div>
   
 
   <div className="column center aligned">
        <h2 className="ui header project-name"><span className="project-name">JavaScript Quiz</span></h2><br/>
                 <img className="project-images" src={quiz} alt="Javascript Quiz" /><br/>
-                <a href="https://stephenfudge.github.io/furry-umbrella/" target="_blank"><span className='project-links'>See it live!</span></a><br/>
-                <a href="https://github.com/stephenfudge/furry-umbrella" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
+                <a href="https://stephenfudge.github.io/furry-umbrella/" target="_blank" rel="noopener noreferrer"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://github.com/stephenfudge/furry-umbrella" target="_blank" rel="noopener noreferrer"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
      </div>
 
  
@@ -61,8 +61,8 @@ export default function Projects() {
   <div className="column center aligned">
        <h2 className="ui header project-name"><span className="project-name">FIFA Fan Site</span></h2><br/>
                 <img className="project-images" src={fifa} alt="FIFA Fan Site" /><br/>
-                <a href="https://vigilant-octo-lamp.herokuapp.com/" target="_blank"><span className='project-links'>See it live!</span></a><br/>
-                <a href="https://github.com/stephenfudge/vigilant-octo-lamp" target="_blank"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
+                <a href="https://vigilant-octo-lamp.herokuapp.com/" target="_blank" rel="noopener noreferrer"><span className='project-links'>See it live!</span></a><br/>
+                <a href="https://github.com/stephenfudge/vigilant-octo-lamp" target="_blank" rel="noopener noreferrer"><span className='project-links'>Take a look at the GitHub Repository</span></a> 
       </div>
 
 
