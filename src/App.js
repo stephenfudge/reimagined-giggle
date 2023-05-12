bedc482d597dc651e28bf6af255d79126cb42a90
Stephen Fudge <sfudge@gmail.com>
2023-05-12 16:02:12 -0400

    added code in App.js to redirect to new portfolio

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
+// import './App.css';
+
+// import { useState } from 'react';
+
+// import Nav from './components/Nav';
+// import Footer from './components/Footer';
+// import AboutMe from './pages/AboutMe';
+// import Projects from './pages/Projects';
+// import Resume from './pages/Resume';
+
+// function App() {
+//   const [page, setPage] = useState('about');
+
+//   function RenderPage(){
+//     switch(page){
+//       case "about":
+//         return <AboutMe />
+//          case "projects":
+//             return <Projects />
+//             case "resumes":
+//               return <Resume />
+//               default:
+//                return <AboutMe />
+//     }
+
+//   }
+//   return (
+//     <div className="App">
+//     <Nav page={page} setPage={setPage}/>
+//   <RenderPage />
+//   <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useEffect } from "react";
 import './App.css';
 
-import { useState } from 'react';
-
-
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import AboutMe from './pages/AboutMe';
-import Projects from './pages/Projects';
-import Resume from './pages/Resume';
-
-
-function App() {
-  const [page, setPage] = useState('about');
-
-  function RenderPage(){
-    switch(page){
-      case "about":
-        return <AboutMe />
-         case "projects":
-            return <Projects />
-            case "resumes":
-              return <Resume />
-              default:
-               return <AboutMe />
-    } 
-
-
-  }
+export default function App() {
+ 
+  useEffect(()=>{
+    window.location.href="https://stephenfudge-portfolio.vercel.app";
+  },[])
   return (
-    <div className="App">
-    <Nav page={page} setPage={setPage}/>
-  <RenderPage />
-  <Footer />
+    <div>
+      <h1>Redirecting to my new portfolio...</h1>
     </div>
   );
 }
-
-export default App;
